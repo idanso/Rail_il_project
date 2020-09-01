524e2eaeafc8576c62f736b92e67023427d0e726
idanso <sorany123@gmail.com>
2020-09-01 19:45:08 +0300

    added frequency lines

--- src/railIl/Line.java
+++ src/railIl/Line.java
@@ -1,5 +1,6 @@
 package railIl;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -8,44 +9,20 @@ public class Line {
 	private ArrayList<Route> allStops;
 
 	public Line(ArrayList<Route> allStops) {
-		super();
 		this.allStops = allStops;
 	}
+	public Line(ArrayList<Route> allStops , int hourDelay, int minutesDelay) {//constructor with time delay
+		this.allStops = new ArrayList<>();
+		for (Route r : allStops) {
+			this.allStops.add(new Route(r.getDepartureTime().plusHours(hourDelay).plusMinutes(minutesDelay),r.getArrivalTime().plusHours(hourDelay).plusMinutes(minutesDelay),r.getDeparturePlace(),r.getArrivalPlace()));
+		}
+	}
 
-	public Line() {
-		Scanner s = new Scanner(System.in);
-		allStops = new ArrayList<Route>();
-		int stationCounter = 1;
-		System.out.println("input details of station number " + stationCounter + " :" );
-		allStops.add(new Route());
-		System.out.println("stop number " + stationCounter + " added successfully");
-		stationCounter++;
-		boolean bAnotherStation = true, bError = true;
-		do {
-			System.out.println("input details of station number " + stationCounter + " :" );
-			allStops.add(new Route(allStops.get(stationCounter-2).getArrivalPlace(), allStops.get(stationCounter-2).getArrivalTime()));
-			System.out.println("stop number " + stationCounter + " added successfully");
-			stationCounter++;
-			System.out.println("do you want to add another station to the line? (Y/N)");
-			char answer;
-				do {
-					try {
-						answer = s.next().charAt(0);
-						if(answer == 'n' || answer == 'N')
-							bAnotherStation = false;
-						else if(answer == 'y' || answer == 'Y') {}
-						else
-							throw new Exception("input can only be 'y' || 'Y' || 'n' || 'N'");
-						bError = false;
-					}
-					catch(Exception e) {
-						System.out.println("please type only 'Y' (yes) / 'N' (no)... please try again");
-					}
-				}
-				while(bError);	
-			}
-		while(bAnotherStation);
-		System.out.println("added new line to the system from " + allStops.get(0).getDeparturePlace() + "to " + allStops.get(allStops.size() - 1).getArrivalPlace());
+	public Route getLastStop() {
+		return allStops.get(allStops.size() - 1);
+	}
+	public ArrayList<Route> getAllStops(){
+		return allStops;
 	}
 
 	@Override
--- src/railIl/Main.java
+++ src/railIl/Main.java
@@ -1,6 +1,8 @@
 package railIl;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -10,14 +12,13 @@ public class Main {
 		ArrayList<Route> allRoutes = new ArrayList<>();//check if still needed
 		ArrayList<Line> allLines = new ArrayList<>();
 		int choice;
-		System.out.println("Enter your choice: ");
-		for (int i = 1; i <= 4; i++) {
-			System.out.println("[" + i + "]" + "-" + MenuHelper(i - 1));
-		}
-		System.out.println("[" + 9 + "]-To exit");
 		
 		do {
-
+			System.out.println("Enter your choice: ");
+			for (int i = 1; i <= 4; i++) {
+				System.out.println("[" + i + "]" + "-" + MenuHelper(i - 1));
+			}
+			System.out.println("[" + 9 + "]-To exit");
 			choice = s.nextInt();
 
 			switch (choice) {
@@ -33,8 +34,62 @@ public class Main {
 				break;
 			}
 			case 3: {
-				allLines.add(new Line());
-				System.out.println("new Line added succefully");
+				ArrayList<Route> allStops  = new ArrayList<Route>(); 
+				int stationCounter = 1;
+				boolean bExceptionFree = true;
+				int hourDelay = 0, minutesDelay = 0;
+				do {
+					try {
+						System.out.println("what the frequency of the line you want?(hours and minutes)\n"
+								+ "type hour frequency(0 for none): ");
+						hourDelay = s.nextInt();
+						System.out.println("type minutes frequency(0 for none): ");
+						minutesDelay = s.nextInt();
+						s.nextLine();
+						bExceptionFree = false;
+					} catch (InputMismatchException e) {
+						System.out.println("entered invalid input, try again...");
+					}
+				}
+				while(bExceptionFree);
+				System.out.println("input details (if frequent, the time is for the first line of the day) of station number " + stationCounter + " :" );
+				allStops.add(new Route());
+				System.out.println("stop number " + stationCounter + " added successfully");
+				stationCounter++;
+				boolean bAnotherStation = true, bError = true;
+				do {
+					System.out.println("input details of station number " + stationCounter + " :" );
+					allStops.add(new Route(allStops.get(stationCounter-2).getArrivalPlace(), allStops.get(stationCounter-2).getArrivalTime()));
+					System.out.println("stop number " + stationCounter + " added successfully");
+					stationCounter++;
+					System.out.println("do you want to add another station to the line? (Y/N)");
+					char answer;
+						do {
+							try {
+								answer = s.next().charAt(0);
+								if(answer == 'n' || answer == 'N')
+									bAnotherStation = false;
+								else if(answer == 'y' || answer == 'Y') {}
+								else
+									throw new Exception("input can only be 'y' || 'Y' || 'n' || 'N'");
+								bError = false;
+							}
+							catch(Exception e) {
+								System.out.println("please type only 'Y' (yes) / 'N' (no)... please try again");
+							}
+						}
+						while(bError);	
+					}
+				while(bAnotherStation);
+				allLines.add(new Line(allStops));
+				if(hourDelay != 0 || hourDelay !=0) {
+					while((allLines.get(allLines.size() - 1).getLastStop().getDepartureTime().getHour() + hourDelay) + (allLines.get(allLines.size() - 1).getLastStop().getArrivalTime().getHour() - allLines.get(allLines.size() - 1).getLastStop().getDepartureTime().getHour()) < 23 && (allLines.get(allLines.size() - 1).getLastStop().getDepartureTime().getMinute() + hourDelay) + (allLines.get(allLines.size() - 1).getLastStop().getArrivalTime().getMinute() - allLines.get(allLines.size() - 1).getLastStop().getDepartureTime().getMinute()) < 59){
+						allLines.add(new Line(allLines.get(allLines.size() - 1).getAllStops(),hourDelay,minutesDelay));
+					}
+				}
+					
+				System.out.println();
+				System.out.println("added new lines to the system from " + allStops.get(0).getDeparturePlace() + " to " + allStops.get(allStops.size() - 1).getArrivalPlace() + " with frequency: " + hourDelay + ":" + minutesDelay);
 				break;
 			}
 			case 4: {
